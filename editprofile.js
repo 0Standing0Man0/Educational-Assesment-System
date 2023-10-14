fbd536293b9c9e80c0025523da7a457bc14d3275
Shubhajit Biswas <shubhajitbiswas94@gmail.com>
2023-10-14 11:40:43 +0530

    editing- edit profile page

--- editprofile.js
+++ editprofile.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const studentNameInput = document.getElementById('studentName');
     const studentEmailInput = document.getElementById('studentEmail');
     const studentGradeInput = document.getElementById('studentGrade');
+    const studentPictureInput = document.getElementById('studentPicture');
 
     // Fetch student's current profile data from the server and populate the form fields
 
@@ -14,7 +15,15 @@ document.addEventListener('DOMContentLoaded', function() {
             name: studentNameInput.value,
             email: studentEmailInput.value,
             grade: studentGradeInput.value,
+            picture: studentPictureInput.files[0], // Uploaded image file
         };
+
+        // Create a FormData object to send the data to the server
+        const formData = new FormData();
+        formData.append('name', updatedProfile.name);
+        formData.append('email', updatedProfile.email);
+        formData.append('grade', updatedProfile.grade);
+        formData.append('picture', updatedProfile.picture);
 
         // Send the updated profile data to the server for processing
         // You would typically use AJAX or fetch for this
