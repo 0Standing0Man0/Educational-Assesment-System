9c4b8dec4c08835360e115abf41c81c23cdb51b0
Shubhajit Biswas <shubhajitbiswas94@gmail.com>
2023-10-15 22:56:33 +0530

    updating documentstud for js and html

--- js/documentstud.js
+++ js/documentstud.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const filterTypeSelect = document.getElementById('filterType');
     const searchBox = document.getElementById('searchBox');
     const filterButton = document.getElementById('filterButton');
+    const downloadLinks = document.querySelectorAll('.download-link');
+    const previewLinks = document.querySelectorAll('.preview-link');
     const documentItems = document.querySelectorAll('.document-list li');
 
     // Event listener for filtering and searching
@@ -20,6 +22,35 @@ document.addEventListener('DOMContentLoaded', function() {
                 item.style.display = 'block';
             } else {
                 item.style.display = 'none';
+            }
+        });
+    });
+
+    // Event listener for downloading documents
+    downloadLinks.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const documentLink = event.target.getAttribute('href');
+            if (documentLink) {
+                // Trigger download by clicking on the hidden link
+                const hiddenLink = document.createElement('a');
+                hiddenLink.href = documentLink;
+                hiddenLink.download = '';
+                hiddenLink.style.display = 'none';
+                document.body.appendChild(hiddenLink);
+                hiddenLink.click();
+                document.body.removeChild(hiddenLink);
+            }
+        });
+    });
+
+    // Event listener for previewing documents
+    previewLinks.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const documentLink = event.target.getAttribute('href');
+            if (documentLink) {
+                window.open(documentLink, '_blank');
             }
         });
     });
