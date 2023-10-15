d9631e539393ae192ce251b8443af4f962d480d6
Shubhajit Biswas <shubhajitbiswas94@gmail.com>
2023-10-15 22:08:42 +0530

    editing edit profile page

--- js/editprofile.js
+++ js/editprofile.js
@@ -2,8 +2,14 @@ document.addEventListener('DOMContentLoaded', function() {
     const profileForm = document.getElementById('profileForm');
     const studentNameInput = document.getElementById('studentName');
     const studentEmailInput = document.getElementById('studentEmail');
-    const studentGradeInput = document.getElementById('studentGrade');
     const studentPictureInput = document.getElementById('studentPicture');
+    const studentPhonenoInput = document.getElementById('studentPhoneno');
+    const studentDobInput = document.getElementById('studentDob');
+
+    // Add an event listener to the phone number input for validation
+    studentPhonenoInput.addEventListener("input", function () {
+        this.value = this.value.replace(/\D/g, '').substr(0, 10); // Remove non-digits and limit to 10 digits
+    });
 
     // Fetch student's current profile data from the server and populate the form fields
 
@@ -14,16 +20,18 @@ document.addEventListener('DOMContentLoaded', function() {
         const updatedProfile = {
             name: studentNameInput.value,
             email: studentEmailInput.value,
-            grade: studentGradeInput.value,
             picture: studentPictureInput.files[0], // Uploaded image file
+            phoneno: studentPhonenoInput.value,
+            dob: studentDobInput.value
         };
 
         // Create a FormData object to send the data to the server
         const formData = new FormData();
         formData.append('name', updatedProfile.name);
         formData.append('email', updatedProfile.email);
-        formData.append('grade', updatedProfile.grade);
         formData.append('picture', updatedProfile.picture);
+        formData.append('phoneno', updatedProfile.phoneno);
+        formData.append('dob', updatedProfile.dob);
 
         // Send the updated profile data to the server for processing
         // You would typically use AJAX or fetch for this
